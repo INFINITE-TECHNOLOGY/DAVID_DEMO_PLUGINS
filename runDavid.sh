16ff090a01aae4ac7717b8e4811ab6aa44f0cc17
apryamostanov <anton.pryamostanov@gmail.com>
2020-03-03 17:53:13 +0400

    Progress

--- runDavid.sh
+++ runDavid.sh
@@ -3,5 +3,5 @@ java \
  -Dloader.path="build/libs" \
  -Dspring.config.location="build/conf/application.properties" \
  $JAVA_OPTS \
- -cp "build/libs/david-app-1.0.0.jar" \
+ -cp "build/libs/david-app-1.0.1.jar" \
  org.springframework.boot.loader.PropertiesLauncher
